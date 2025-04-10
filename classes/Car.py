f0221892053e10beb0ac9cc3dd7f5e964732c49f
antoinayaya <antoine.mauroit@gmail.com>
2025-04-10 09:17:51 +0200

    feat : suiveur de mur droite

--- classes/Car.py
+++ classes/Car.py
@@ -1,11 +1,11 @@
 from classes.Sensor_Manager import Sensor_Manager
-from classes.MotorManager import MotorManager
+from classes.Motor_Manager import Motor_Manager
 import busio
 import board
 import time
 
 class Car:
-    def __init__(self,car_name:str,motor_manager:MotorManager,sensor_manager:Sensor_Manager,const_config:dict):
+    def __init__(self,car_name:str,motor_manager:Motor_Manager,sensor_manager:Sensor_Manager,const_config:dict):
         self._car_name = car_name
         print(f"""
 Initialisation de la voiture : {self._car_name}
@@ -14,7 +14,7 @@ Initialisation de la voiture : {self._car_name}
         self._motor_manager = motor_manager
         self._total_laps = int(0)
         self._last_lap_duration = int(0)
-        self._current_state = "standBy"
+        self._current_state = "stand_by"
         self._const_config = const_config
         print(f"""
 {self._car_name} opérationel !
@@ -67,30 +67,28 @@ Initialisation de la voiture : {self._car_name}
           5. On met à jour la direction et la vitesse via le motorManager.
         """
         front_disc, left_disc, right_disc = distances
-        if front_disc is None or front_disc < self._const_config["OBSTACLE_MINIMUM_DIST"]:
-            self._motor_manager.stopMotors()
-            return (0, 0)
+        if right_disc < 0:
+            raise ValueError("right_disc cannot be negative")
+        if right_disc > 100:
+            right_disc = 100
+        
+        if right_disc < 10 :
+            new_direction = (10 - right_disc * 10)*-1
+            new_speed = 10 - right_disc * 10
+        elif right_disc > 10 :
+            new_direction = right_disc /2
+            new_speed = 100
+        else:
+            new_direction = 0
+            new_speed = 100
+        return (new_direction, new_speed)
+            
 
-        Kp = 10  # facteur de gain (à ajuster par calibration)
-        error = right_disc - left_disc  # Si positif, la voiture est plus proche du mur de gauche et doit tourner à droite.
-        new_direction = max(-100, min(100, Kp * error))
-
-        min_front = self._const_config["OBSTACLE_MINIMUM_DIST"]  
-        max_front = self._const_config["MAX_FRONT_DIST"]
-        new_speed = (front_disc - min_front) / (max_front - min_front) * 100
-        new_speed = max(0, min(100, new_speed))
-        reduction_factor = 1 - (abs(new_direction) / 100) * 0.5  
-        new_speed = new_speed * reduction_factor
-
-        if isLine:
-            self.countLap(isLine)
-
-        self._motor_manager.setAngle(new_direction)
-        self._motor_manager.setSpeed(new_speed, new_speed)
+        
 
         return (new_direction, new_speed)
 
-    def u_turn(self) -> None:
+    def u_turn(self, direction: str) -> None:
         """
         Exécute un U-Turn en braquant à fond (gauche ou droite) pendant une durée donnée,
         à une vitesse spécifiée.
@@ -100,50 +98,54 @@ Initialisation de la voiture : {self._car_name}
           - duration: durée du U-Turn en secondes
           - speed: vitesse en pourcentage (0 à 100) pendant le U-Turn
         """
-        turn_value = -100
+        speed = 80
+        if direction.lower() == 'left':
+            turn_value = -100
+        elif direction.lower() == 'right':
+            turn_value = 100
+        else:
+            raise ValueError("La direction doit être 'left' ou 'right'")
         self._motor_manager.setAngle(turn_value)
-        self._motor_manager.setSpeed(80, 80)
+        self._motor_manager.setSpeed(speed) 
+        time.sleep(4)
+
         self._motor_manager.setSpeed(0)
-        time.sleep(0.2)
-        turn_value = 100
-        self._motor_manager.setAngle(turn_value)
-        self._motor_manager.setSpeed(80, 80)
-        self._motor_manager.setSpeed(0)
+        self._motor_manager.setAngle(0)
 
-        def monitoring(self, distances: tuple, isLine: bool, direction: str, speed: float, ina: dict, rgb: tuple):
-            """
-            Affiche les données en temps réel pour le suivi de l'état de la voiture.
+    def monitoring(self, distances: tuple, isLine: bool, direction: str, speed: float, ina: dict, rgb: tuple):
+        """
+        Affiche les données en temps réel pour le suivi de l'état de la voiture.
 
-            Paramètres :
-            - distances : un tuple (right, front, left) en cm
-            - isLine    : booléen indiquant si la ligne est détectée
-            - direction : direction actuelle de la voiture ('left' ou 'right')
-            - speed     : vitesse de la voiture en pourcentage
-            - ina       : un dictionnaire contenant les données du capteur INA (bus_voltage, shunt_voltage, current)
-            - rgb       : un tuple contenant les valeurs RGB (0-255, 0-255, 0-255)
-            """
-            right_disc, front_disc, left_disc = distances
+        Paramètres :
+        - distances : un tuple (right, front, left) en cm
+        - isLine    : booléen indiquant si la ligne est détectée
+        - direction : direction actuelle de la voiture ('left' ou 'right')
+        - speed     : vitesse de la voiture en pourcentage
+        - ina       : un dictionnaire contenant les données du capteur INA (bus_voltage, shunt_voltage, current)
+        - rgb       : un tuple contenant les valeurs RGB (0-255, 0-255, 0-255)
+        """
+        right_disc, front_disc, left_disc = distances
 
-            # Affichage des données
-            print(f"""
-            ======= État actuel de la voiture : {self._car_name} =======
-            Distance droite: {right_disc} cm
-            Distance frontale: {front_disc} cm
-            Distance gauche: {left_disc} cm
-            Ligne détectée: {'Oui' if isLine else 'Non'}
-            Direction: {direction}
-            Vitesse: {speed} %
+        # Affichage des données
+        print(f"""
+        ======= État actuel de la voiture : {self._car_name} =======
+        Distance droite: {right_disc} cm
+        Distance frontale: {front_disc} cm
+        Distance gauche: {left_disc} cm
+        Ligne détectée: {'Oui' if isLine else 'Non'}
+        Direction: {direction}
+        Vitesse: {speed} %
 
-            === Données du capteur INA ===
-            Bus Voltage: {ina['BusVoltage']} V
-            Shunt Voltage: {ina['Shunt Voltage']} V
-            Current: {ina['Current']} A
+        === Données du capteur INA ===
+        Bus Voltage: {ina['BusVoltage']} V
+        Shunt Voltage: {ina['Shunt Voltage']} V
+        Current: {ina['Current']} A
 
-            === Couleur RGB ===
-            R: {rgb[0]} G: {rgb[1]} B: {rgb[2]}
+        === Couleur RGB ===
+        R: {rgb[0]} G: {rgb[1]} B: {rgb[2]}
 
-            ============================================================
-            """)
+        ============================================================
+        """)
 
 
 
