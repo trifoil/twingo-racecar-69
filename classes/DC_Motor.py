ee4c9168f6471eacb38cd7316d7fb7f21fa3edd0
Mathis-Kruczynski <mathis.kruczynski@std.heh.be>
2025-04-11 10:30:14 +0200

    fix : commentaire et docstring DC_Motor

--- classes/DC_Motor.py
+++ classes/DC_Motor.py
@@ -1,24 +1,27 @@
 """
+La Classe DC Motor va nous permettre d'instancier les DC motor et de sortir les informations utiles afin
+de les controler dans un manager.
 Classe DC Motor va comprendre tous les attributs utile à la classe et qui seront repris dans
-le motor manager
+le motor manager pour le contrôle.
 """
 
 
 import RPi.GPIO as GPIO
 
-"""
-Constructeur qui va comprendre :
-pin_enable = pin reliée au PWM
-pin_input = pins reliés au pont en H qui permettent de gèrer la vitesse
-
-
-GPIO setup permet de setup le mode IN/OUT des pins. 
-Ici, toutes les pins sont en sortie (OUT) car ils renvoient leurs infos.
-Le setup High ou Low servira a définir le sens de rotation (accélération ou freinage)
-"""
 
 
 class DC_Motor():
+    """
+    Constructeur qui va comprendre :
+    pin_enable = pin reliée au PWM
+    pin_input = pins reliés au pont en H qui permettent de gèrer la vitesse
+
+
+    GPIO setup permet de setup le mode IN/OUT des pins.
+    Ici, toutes les pins sont en sortie (OUT) car ils renvoient leurs infos.
+    Le setup High ou Low servira a définir le sens de rotation (accélération ou freinage)
+    """
+
     def __init__(self, pin_enable: int, pin_input1: int, pin_input2: int):
         assert (pin_enable not in [pin_input1, pin_input2]) and (pin_input1 != pin_input2)
         for pin in [pin_enable, pin_input1, pin_input2]:
@@ -33,7 +36,7 @@ class DC_Motor():
         GPIO.setup(self._pin_input2, GPIO.OUT)
 
     """
-    Set up de getter nécessaire de tout les attributs du DC_Motor
+    listes des set up de getter nécessaires de tout les attributs du DC_Motor
     """
 
     @property
@@ -48,11 +51,13 @@ class DC_Motor():
     def pin_input2(self) -> int:
         return self._pin_input2
 
-    """
-    Fonction qui permet de gérer l'accélération ou freinage.
-    """
+
 
     def set_direction(self, forward: bool) -> None:
+        """
+        Fonction qui permet de gérer l'accélération ou freinage.
+        Elle fonctionne sur un booléen. Si le booléen est True, elle avance. Si False, elle freine.
+        """
         if forward:
             GPIO.output(self.pin_input1, GPIO.HIGH)
             GPIO.output(self.pin_input2, GPIO.LOW)
@@ -60,22 +65,24 @@ class DC_Motor():
             GPIO.output(self.pin_input1, GPIO.LOW)
             GPIO.output(self.pin_input2, GPIO.HIGH)
 
-    """
-    Fonction stop qui met la voiture à l'arrêt en la faisant freiné.
-    En mettant les deux pins en HIGH, le moteur va freiné pour un arrêt rapide.
-    """
+
 
     def stop_brake(self) -> None:
+        """
+        Fonction stop qui met la voiture à l'arrêt en la faisant freiné.
+        En mettant les deux pins en HIGH, le moteur va freiné pour un arrêt rapide.
+        """
         GPIO.output(self.pin_input1, GPIO.HIGH)
         GPIO.output(self.pin_input2, GPIO.HIGH)
 
-    """
-    Fonction stop qui met la voiture en l'arrêt en coupant le moteur.
-    En mettant les deux pins en LOW, le moteur va se mettre à l'arrêt.
-    La voiture va continuer jusqu'a l'arrêt complet (freinage par frottement)
-    """
+
 
     def stop_free_wheels(self) -> None:
+        """
+        Fonction stop qui met la voiture en l'arrêt en coupant le moteur.
+        En mettant les deux pins en LOW, le moteur va se mettre à l'arrêt.
+        La voiture va continuer jusqu'a l'arrêt complet (freinage par frottement)
+        """
         GPIO.output(self.pin_input1, GPIO.LOW)
         GPIO.output(self.pin_input2, GPIO.LOW)
 
